630e1bb3568c69f07192e97644f074d7df8a5dfb
JM-alpha8 <ngkjmgb@gmail.com>
2025-05-30 08:51:01 +0900

    Refactor: v4

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify, render_template
 from flask_mail import Mail, Message
 import base64
-import io
 
-app = Flask(__name__, static_url_path='/models', static_folder='static/models')
+app = Flask(__name__, static_url_path='/models', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pages/<path:filename>')
+def load_page(filename):
+    return send_from_directory('templates/pages', filename)
+
+@app.route('/scripts/<path:filename>')
+def serve_script(filename):
+    return send_from_directory('static/scripts', filename)
+
+@app.route('/static/images/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('static/images', filename)
+
+@app.route('/static/sounds/<path:filename>')
+def serve_sounds(filename):
+    return send_from_directory('static/sounds', filename)
 
 @app.route('/icons/<path:filename>')
 def icons(filename):
@@ -17,73 +36,13 @@ def manifest():
 def service_worker():
     return send_from_directory('static', 'serviceWorker.js')
 
-@app.route('/pages/<path:filename>')
-def load_page(filename):
-    return send_from_directory('templates/pages', filename)
-
-@app.route('/static/images/<path:filename>')
-def serve_image(filename):
-    return send_from_directory('static/images', filename)
-
-@app.route('/scripts/<path:filename>')
-def serve_script(filename):
-    return send_from_directory('static/scripts', filename)
-
-# ✅ 페이지 라우팅
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/game_follow')
-def game_follow():
-    return render_template('game_follow.html')  # 표정 따라하기 모드
-
-@app.route('/game_feedback')
-def game_feedback():
-    return render_template('game_feedback.html')
-
-@app.route('/rehab_mode')
-def rehab_mode():
-    return render_template('rehab_mode.html')
-
-@app.route('/focus')
-def focus():
-    return render_template('focus.html')
-
-@app.route('/complex')
-def complex():
-    return render_template('complex.html')
-
-@app.route('/complex_fit')
-def complex_fit():
-    return render_template('complex_fit.html')
-
-@app.route('/focus_fit')
-def focus_fit():
-    return render_template('focus_fit.html')
-
-@app.route('/feedback')
-def feedback():
-    return render_template('feedback.html')
-
-@app.route('/game_mode')
-def game_mode():
-    return render_template('game_mode.html')
-
-@app.route('/game_emotion')
-def game_emotion():
-    return render_template('game_emotion.html')
-
-@app.route('/static/sounds/<path:filename>')
-def serve_sounds(filename):
-    return send_from_directory('static/sounds', filename)
-
+# 이메일 기능 유지
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=587,
     MAIL_USE_TLS=True,
     MAIL_USERNAME='your_email@gmail.com',
-    MAIL_PASSWORD='your_app_password',  # 앱 비밀번호 사용 (Google 계정 보안 설정 필요)
+    MAIL_PASSWORD='your_app_password',
     MAIL_DEFAULT_SENDER='your_email@gmail.com'
 )
 
@@ -98,7 +57,6 @@ def send_email():
     msg = Message('SMILE FIT 결과 이미지', recipients=[email])
     msg.body = "첨부된 사진은 SMILE FIT에서 촬영된 결과입니다."
 
-    # 이미지 첨부
     for i, img_data_url in enumerate(images):
         header, base64_data = img_data_url.split(',', 1)
         img_bytes = base64.b64decode(base64_data)
